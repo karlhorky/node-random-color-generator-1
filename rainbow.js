94b8cc21ce3c95f0775ace5fc4172ed1b9157f2e
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2020-09-08 18:17:19 +0300

    Main2 commit

--- rainbow.js
+++ rainbow.js
@@ -2,11 +2,14 @@ const randomColor = require('randomcolor');
 //let color = randomColor();
 const colors = require('colors');
 const chalk = require('chalk');
-const hue_c = process.argv[2];
-const hue_l = process.argv[3];
+let hue_c = '';
+let hue_l = '';
+//const hue_c = process.argv[2];
+//const hue_l = process.argv[3];
 let str = '';
 let str1 = '';
 let str2 = '';
+
 //init string
 for (let i = 0; i < 31; i++) {
   str += '#';
@@ -34,6 +37,12 @@ for (let i = 0; i < 8; i++) {
   str2 += '#';
 }
 //print
+if (process.argv[2] != '') {
+  hue_c = process.argv[2];
+}
+if (process.argv[3] != '') {
+  hue_l = process.argv[3];
+}
 const color1 = randomColor({
   hue: hue_c,
   luminosity: hue_l,
@@ -44,6 +53,7 @@ const color1 = randomColor({
 //} else {
 //  ('Print please name of the color');
 //}
+
 for (let i = 0; i < 3; i++) {
   console.log(chalk.hex(color1)(str));
 }
